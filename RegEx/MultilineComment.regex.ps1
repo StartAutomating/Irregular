60738cc0a364c7e6bc458b91fb2d075faccb37b7
StartAutomating <james.brundage@start-automating.com>
2019-10-26 15:16:02 -0700

    * New Expressions: ?<BuildVersion> and ?<PHP_Tag> * ?<MultilineComment> now finds C-style comments in PHP files * Bumping module version

--- RegEx/MultilineComment.regex.ps1
+++ RegEx/MultilineComment.regex.ps1
@@ -9,14 +9,14 @@ When this generator is used with a piped in file, the extension will autodetect 
 If the format could not be autodetected, the match will always fail.
 #>
 param(
-[ValidateSet('PowerShell', 'C#', 'C++', 'C', 'JavaScript', 'JSON', 'Java', 'Ruby', 'HTML', 'XML','')]
+[ValidateSet('PowerShell', 'C#', 'C++', 'C', 'JavaScript', 'JSON', 'Java', 'Ruby', 'HTML', 'XML','PHP','')]
 [string]
 $Language = 'C'
 )
 
 if ($inputObject -and $inputObject -is [IO.FileInfo]) {
     $Language = 
-        if ('.h', '.cpp', '.c', '.cs', '.js', '.java','.json' -contains $inputObject.Extension) {
+        if ('.h', '.cpp', '.c', '.cs', '.js', '.java','.json','.php' -contains $inputObject.Extension) {
             'C'
         } elseif ('.ps1', '.psm1', '.psd1' -contains $inputObject.Extension) {
             'PowerShell'
