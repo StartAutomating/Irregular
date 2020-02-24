562d7acb916468d7e912eb65aae65683e0f8c01b
James Brundage <james.brundage@start-automating.com>
2020-02-23 23:29:48 -0800

    Adding ?<PowerShell_Invoke_Variable> and source

---
+++ RegEx/PowerShell/Invoke_Variable.regex.source.ps1
@@ -0,0 +1,13 @@
+﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+Write-RegEx -Description @'
+Matches any time a variable is invoked (with the . or & operator)
+'@ -NotAfter '[\w\)`]' -Comment 'If the text before the invoke is a word, closing paranthesis, or backtick, do not match' | 
+    Write-RegEx -LiteralCharacter '.&' -Name CallOperator -Comment "Match the <CallOperator> (either a . or a &)"  |
+    Write-RegEx -CharacterClass Whitespace -Min 0 -Comment "Followed by Optional Whitespace" |
+    Write-RegEx -LiteralCharacter '$' -Comment "Followed by a Dollar Sign" |
+    Write-RegEx -Pattern @(
+        Write-RegEx -CharacterClass Word -Repeat -Name Variable -Comment 'Followed by a <Variable> (any number of repeated word characters)'
+        Write-RegEx -Between '{','}' -Name Variable -EscapeSequence '`' -Description 'Or a <Variable> enclosed in curly brackets' -Comment 'using backtick as an escape'
+    ) -Or |
+    Set-Content -Path (Join-Path $myRoot $myName)
