c88e921c32df139c522f6f3e0dd3c62349b2f624
James Brundage <james.brundage@start-automating.com>
2020-10-07 13:05:26 -0700

    Adding ?<Git_Diff> (and source).

---
+++ RegEx/Git/Diff.regex.source.ps1
@@ -0,0 +1,11 @@
+﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+
+Write-RegEx -Pattern '(?m)' |
+Write-RegEx -Pattern '?<Git_DiffHeader>' |
+    Write-RegEx -Until (
+        Write-RegEx 'diff' -StartAnchor LineStart
+    ) -Name Git_DiffRanges |
+    Set-Content -Path (Join-Path $myRoot $myName)
+
+
