11a4b4fc32e86908ccca419d1d7a20d1d1cb5224
James Brundage <james.brundage@start-automating.com>
2020-10-08 11:17:27 -0700

    ?<Git_Diff>/?<Git_DiffRange>: Adding descriptions.

--- RegEx/Git/Diff.regex.source.ps1
+++ RegEx/Git/Diff.regex.source.ps1
@@ -1,7 +1,7 @@
 ﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
 $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 
-Write-RegEx -Pattern '(?m)' |
+Write-RegEx -Pattern '(?m)' -Description "Matches Output from git diff" |
 Write-RegEx -Pattern '?<Git_DiffHeader>' |
     Write-RegEx -Until (
         Write-RegEx 'diff' -StartAnchor LineStart
--- RegEx/Git/DiffRange.regex.source.ps1
+++ RegEx/Git/DiffRange.regex.source.ps1
@@ -1,15 +1,15 @@
 ﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
 $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 
-Write-RegEx -Pattern '(?m)' |
+Write-RegEx -Pattern '(?m)' -Description "Matches a diff range" |
     Write-RegEx -LiteralCharacter '@' -Min 2 -Max 2 -StartAnchor LineStart -NoCapture -Comment 'Two @s' |
     Write-RegEx -CharacterClass Whitespace -Min 1 -Comment Whitespace |
-    Write-RegEx -LiteralCharacter '-' -Comment Dash |
+    Write-RegEx -LiteralCharacter '-' -Comment Dash | 
     Write-RegEx -Name FromFileLineStart -CharacterClass Digit -Repeat -Comment From |
     Write-RegEx -LiteralCharacter ',' -Comment Comma  |
     Write-RegEx -Name FromFileLineCount -CharacterClass Digit -Repeat -Optional -Comment LineCount |
     Write-RegEx -CharacterClass Whitespace -Min 1 -Comment Whitespace |
-    Write-RegEx -LiteralCharacter '+' -Comment Plus |
+    Write-RegEx -LiteralCharacter '+' -Comment Plus | 
     Write-RegEx -Name ToFileLineEnd -CharacterClass Digit -Repeat -Comment To |
     Write-RegEx -LiteralCharacter ',' |
     Write-RegEx -Name ToFileLineCount -CharacterClass Digit -Repeat -Optional -Comment LineCount |
