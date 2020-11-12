ac4a2e81cf9c5c76729ad462dafdd68053010eeb
James Brundage <james.brundage@start-automating.com>
2020-11-11 17:49:04 -0800

    Adding ?<Unix_FileSystemType>

---
+++ RegEx/Unix/FileSystemType.regex.source.ps1
@@ -0,0 +1,11 @@
+﻿$myFile = $MyInvocation.MyCommand.ScriptBlock.File
+$myName = ($myFile | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $myFile | Split-Path
+
+Write-RegEx -StartAnchor LineStart -Modifier Multiline -Description 'Matches a File System Type (described in /proc/filesystems)' |
+    Write-RegEx -Pattern nodev -Optional -Name NoDevice |
+    Write-RegEx -CharacterClass Whitespace -Greedy -Lazy |
+    Write-RegEx -Name FileSystemName -CharacterClass NonWhitespace -Repeat |
+    Write-RegEx -Pattern '.*$' -NoCapture -Comment 'Then match until the end of the line.' |
+    Set-Content -Path (Join-Path $myRoot $myName)
+
