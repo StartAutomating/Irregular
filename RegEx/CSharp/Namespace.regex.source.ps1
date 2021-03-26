315d1855713fbbacf62362bf0285910f08069d58
James Brundage <james.brundage@start-automating.com>
2021-03-25 23:05:51 -0700

    Adding ?<CSharp_Namespace> (calling down to ?<Namespace>)

---
+++ RegEx/CSharp/Namespace.regex.source.ps1
@@ -0,0 +1,4 @@
+﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+Write-RegEx -Description 'Matches a CSharp namespace' -Pattern '?<Namespace>(CSharp)'|    
+    Set-Content -Path (Join-Path $myRoot $myName) -Encoding UTF8
