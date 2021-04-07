fab0286ab8a0e1ab2235b4ed0d93e10f471d92b3
James Brundage <james.brundage@start-automating.com>
2021-04-07 12:20:49 -0700

    Adding GitHubWorkflow definition file

---
+++ Irregular.GitHubWorkflow.PSDevops.ps1
@@ -0,0 +1,3 @@
+﻿#requires -Module PSDevOps
+New-GitHubWorkflow -Name IrregularTests -Job TestPowerShellOnLinux -On Push, Demand |
+    Set-Content .\.github\workflows\IrregularTests.yml -Encoding UTF8
