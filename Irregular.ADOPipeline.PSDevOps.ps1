c8d4c9be10e32fb5b6acbd880ee1d73010efa886
James Brundage <james.brundage@start-automating.com>
2021-04-07 12:20:14 -0700

    Adding #requires to ADOPipeline generator.

--- Irregular.ADOPipeline.PSDevOps.ps1
+++ Irregular.ADOPipeline.PSDevOps.ps1
@@ -1,3 +1,9 @@
-﻿#requires -Module PSDevOps
+﻿<#
+.Synopsis
+    
+.Description
+    
+#>
+#requires -Modules PSDevOps
 New-ADOPipeline -Stage PowerShellStaticAnalysis, TestPowerShellCrossPlatform, UpdatePowerShellGallery |
     Set-Content .\azure-pipelines.yml -Encoding UTF8
