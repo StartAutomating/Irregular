79294ef388b4f3335de45bda87b555421c51ae9a
James Brundage <james.brundage@start-automating.com>
2021-04-10 14:35:13 -0700

    Fixing #39 - Updating ?<EmailAddress>- Allowing Subdomains and Adding .regex.source.ps1

--- RegEx/EmailAddress.regex.source.ps1
+++ RegEx/EmailAddress.regex.source.ps1
@@ -4,16 +4,16 @@ $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 Write-RegEx -Description "Matches an Email Address" |
     Write-RegEx -Name UserName -Pattern (
         Write-RegEx -CharacterClass Word -Comment "Match the username, which starts with a word character" |
-            Write-RegEx -CharacterClass Word -LiteralCharacter '-.' -Min 0 -Comment "And can contain any number of word characters, dashes, or dots"
+            Write-RegEx -CharacterClass Word -LiteralCharacter '-.' -Min 0 -Comment "and can contain any number of word characters, dashes, or dots"
     ) |
     Write-RegEx -LiteralCharacter '@' -Comment "Followed by an @"|
     Write-RegEx -Name Domain -Pattern (
         Write-RegEx -CharacterClass Word  -Comment "The domain starts with a word character" |
-            Write-RegEx -CharacterClass Word -LiteralCharacter '-' -Min 0 -Comment "And can contain any words with dashes" |
+            Write-RegEx -CharacterClass Word -LiteralCharacter '-' -Min 0 -Comment "and can contain any words with dashes," |
             Write-RegEx -NoCapture -Pattern (
-                Write-RegEx -LiteralCharacter '.' -Comment "Followed by at least one suffix, which starts with a dot."|
-                    Write-RegEx -CharacterClass Word -Comment "Followed by a word character" |
-                    Write-RegEx -CharacterClass Word -LiteralCharacter '-' -Min 0 -Comment "Followed by any word characters or dashes"
+                Write-RegEx -LiteralCharacter '.' -Comment "followed by at least one suffix (which starts with a dot),"|
+                    Write-RegEx -CharacterClass Word -Comment "followed by a word character," |
+                    Write-RegEx -CharacterClass Word -LiteralCharacter '-' -Min 0 -Comment "followed by any word characters or dashes"
             ) -Min 1
     ) |
     Set-Content -Path (Join-Path $myRoot $myName) -Encoding UTF8
