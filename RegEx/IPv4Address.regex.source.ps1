b7ca4621416822644943af6faf53e4366e34af3c
James Brundage <james.brundage@start-automating.com>
2021-04-10 19:02:27 -0700

    Updating ?<IPv4Address>:  Only allowing byte-values (fixing #38).

--- RegEx/IPv4Address.regex.source.ps1
+++ RegEx/IPv4Address.regex.source.ps1
@@ -2,13 +2,14 @@
 $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 
 Write-RegEx -Description "Matches an IPv4 Address" |    
-    Write-RegEx -CharacterClass Digit -Min 1 -Max 3 -Comment '1-3 digits' |
-    Write-RegEx -LiteralCharacter . -Comment 'a dot' |
-    Write-RegEx -CharacterClass Digit -Min 1 -Max 3 -Comment '1-3 digits' | 
-    Write-RegEx -LiteralCharacter . -Comment 'a dot' |
-    Write-RegEx -CharacterClass Digit -Min 1 -Max 3 -Comment '1-3 digits' | 
-    Write-RegEx -LiteralCharacter . -Comment 'a dot' |
-    Write-RegEx -CharacterClass Digit -Min 1 -Max 3 |    
+    Write-RegEx -DigitMax 255 -Comment 'Match a series of digits (up to 255),'  |
+    Write-RegEx -LiteralCharacter . -Comment 'followed by a dot,' |
+    Write-RegEx -DigitMax 255 -Comment 'followed by another series of digits (up to 255),' | 
+    Write-RegEx -LiteralCharacter . -Comment 'followed by a dot,' |
+    Write-RegEx -DigitMax 255 -Comment 'followed by another series of digits (up to 255),' | 
+    Write-RegEx -LiteralCharacter . -Comment 'folowed by a dot,' |
+    Write-RegEx -DigitMax 255 -Comment 'followed by a final series of digits (up to 255)' | 
+    Write-RegEx -Before '\D|$' -Comment 'followed by a non-digit or end of string.' |
     Set-Content -Path (Join-Path $myRoot $myName)
 
 
