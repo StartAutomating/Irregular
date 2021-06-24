f2bfaead63cfcf96b21870eb3da0cc375c54f914
James Brundage <james.brundage@start-automating.com>
2021-06-23 23:31:39 -0700

    Markdown Expressions and Tests: ?<Markdown_Heading> ?<Markdown_ThematicBreak>

---
+++ RegEx/Markdown/Heading.regex.ps1
@@ -0,0 +1,81 @@
+﻿<#
+.Synopsis
+    Matches Markdown Heading
+.Description
+    Matches Markdown Headings.  Can provide a -HeadingName, -HeadingLevel, and -IncludeContent.
+#>
+param(
+# The name of the heading.  By default, will match any heading
+[Parameter(ValueFromPipelineByPropertyName)]
+[string]
+$HeadingName = '[^#]+?',
+
+# The level(s) of the heading.  If not provided, will match a heading of any level.
+[ValidateRange(0,6)]
+[int[]]
+$HeadingLevel = 0,
+
+# If set, will include the content of the heading.  This will match until the next heading start of any level.
+[switch]
+$IncludeContent
+)
+
+if (-not $HeadingLevel) {
+    $HeadingLevel = 6..1
+}
+
+$HeadingLevel = $HeadingLevel | Sort-Object -Descending
+
+"
+(?m)                           # Set multline mode
+^                              # Anchor at line start
+\s{0,3}                        # Match Up to three whitespace characters, then one of:
+(?>
+"  + (@(
+foreach ($hl in $HeadingLevel) {
+    if ($hl -eq 1) {
+@'
+(?<HeadingName>\S.+?)$         # Match a line followed by
+\s+                            # Some whitespace (in this case, the newline)
+^\s{0,3}(?<h1>=+)\s{0,}$       # A line of all equals
+'@
+    } elseif ($hl -eq 2) {
+@'
+                               # Match SETEXT heading 1:
+(?<HeadingName>\S.+?)$          
+                               # A line of with at least one character of non-whitespace followed by
+\s+                            # Some whitespace (in this case, the newline)
+^\s{0,3}(?<h2>\-+)\s{0,}$      # A line of all dashes (with up to 3 leading spaces)
+'@
+    }
+@"
+                               # Match ATX heading $hl
+(?<h$hl>\#{$hl})               
+\s                             # A single whitespace followed by the heading name
+(?<HeadingName>
+$($HeadingName -replace '\s', '\s')
+)
+(?:[\#\s]{0,})$ # Ignore any remaining #s or whitespace"
+"@
+}
+
+
+) -join ([Environment]::NewLine + (' ' * 4) + '|' + [Environment]::NewLine)) + '
+)
+' + $(
+if ($IncludeContent) {
+@"
+(?<Content>                    # If including Content
+    (?:.|\s){0,}?              # Match anything until
+    (?=
+        ^\s{0,3}               
+        (?>                    # The start of another block
+            [\#\-=~``]         
+        )
+        |                      # or
+        \z                     # the end of the string.
+    )
+)
+"@
+}
+)
---
+++ RegEx/Markdown/Markdown.regex.tests.ps1
@@ -0,0 +1,55 @@
+﻿describe 'Markdown Regexes' {    
+    context 'Headings' {
+        it 'Can match ATX headings' {
+            # See https://github.github.com/gfm/#atx-heading
+            foreach ($n in 1..6) {
+                $headingLine = "$(' ' * (Get-Random -Minimum 0 -Maximum 3))$('#' * $n) h$n"
+                $headingMatched = $headingLine | ?<Markdown_Heading> -Extract 
+                $headingMatched."h$n" | Should -Match "#{$n}"
+                $headingMatched.HeadingName | Should -Be "h$n"
+                $headingMatchedSpecifically = $headingLine | ?<Markdown_Heading> -HeadingLevel $n -Extract
+                $headingMatchedSpecifically."h$n" | Should -Match "#{$n}"
+                $headingMatchedSpecifically.HeadingName | Should -Be "h$n"
+            }
+        }
+
+        # See https://github.github.com/gfm/#setext-heading
+        it 'Can match SETEXT style h1' {
+@"
+  h1
+ ==
+"@ | 
+    ?<Markdown_Heading> -Extract | 
+    ForEach-Object {
+        $_.h1 | Should -BeLike '=*'
+        $_.HeadingName | Should -BeLike 'h1*'
+    }
+        }
+
+        # See https://github.github.com/gfm/#setext-heading
+        it 'Can match SETEXT style h2' {
+@"
+   h2
+----------------------
+"@ | 
+    ?<Markdown_Heading> -Extract |
+    ForEach-Object {
+        $_.h2 | Should -BeLike '-*'
+        $_.HeadingName | Should -BeLike 'h2*'
+    }          
+        }
+    }
+
+    context 'Thematic Breaks' {
+        it 'Will match thematic breaks' {
+            # See https://github.github.com/gfm/#thematic-breaks
+            foreach ($char in '*','-','_') {
+                $rn = Get-Random -Minimum 3 -Maximum 20
+                "$($char * $rn)" | 
+                    ?<Markdown_ThematicBreak> | 
+                    Select-Object -ExpandProperty EndIndex | 
+                    Should -Be $rn
+            }
+        }
+    }
+}
