f06df3f5101c49e13dfda25b29c2228836e12ef8
James Brundage <james.brundage@start-automating.com>
2021-10-23 19:28:51 -0700

    Updating GitHub Workflow (adding tagging, publishing)

--- Irregular.GitHubWorkflow.PSDevops.ps1
+++ Irregular.GitHubWorkflow.PSDevops.ps1
@@ -1,3 +1,3 @@
 ﻿#requires -Module PSDevOps
-New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish -On Push, Demand |
+New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -Job TestPowerShellOnLinux, TagReleaseAndPublish -On Push, Demand |
     Set-Content .\.github\workflows\IrregularTests.yml -Encoding UTF8
