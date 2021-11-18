77de4b2e5c48d02908fada3d790e61a42fdb1ce5
James Brundage <james.brundage@start-automating.com>
2021-11-17 18:52:36 -0800

    Adding OpenScad Regular Expressions:  Fixes #75

---
+++ RegEx/OpenSCAD/Customization.regex.source.ps1
@@ -0,0 +1,25 @@
+﻿#requires -Module Irregular
+$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+
+Write-RegEx -Description "Matches Potential Open SCAD Customizations" -StartAnchor LineStart -Modifier Multiline |        
+    Write-RegEx -CharacterClass Word -Repeat -Name Name -Comment 'Optional Whitespace'   |
+    Write-RegEx -CharacterClass Whitespace -Min 0 -Comment 'Optional Whitespace' |
+    Write-RegEx -LiteralCharacter '=' |
+    Write-RegEx -CharacterClass Whitespace -Min 0 -Comment 'Optional Whitespace' | 
+    Write-RegEx -Name Value -Atomic -Or @(
+        Write-RegEx -Name NumberValue -CharacterClass Digit -Repeat -LiteralCharacter . -Comment "A numeric value"
+        Write-RegEx -Name BooleanValue -Pattern 'true|false' -Comment "A boolean value"
+        Write-RegEx -LiteralCharacter '"'  |
+            Write-RegEx -Name StringValue -Pattern '(?:.|\s)*?(?<!\\)"' -Comment "A string value"
+        Write-RegEx -Name ListValue -Pattern ?<BalancedBrackets> -Comment "A List Value"
+    ) |
+    Write-RegEx -CharacterClass Whitespace -Min 0 -Comment 'Optional Whitespace' |
+    Write-RegEx -LiteralCharacter ';' -Comment 'Semicolon' |
+    Write-RegEx -Name RestOfLine -Pattern .*$ | #>
+    Set-Content -Path (Join-Path $myRoot $myName) -PassThru
+
+
+
+
+
---
+++ RegEx/OpenSCAD/Include.regex.source.ps1
@@ -0,0 +1,12 @@
+﻿#requires -Module Irregular
+$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+
+Write-RegEx -Description "Matches Open SCAD Include statements" |
+    Write-RegEx -Modifier Multiline -Comment 'Set Multiline mode.  Then,' |
+    Write-RegEx -Pattern include -StartAnchor LineStart -Comment "match the literal 'include'" |
+    Write-RegEx -CharacterClass Whitespace -Repeat -Comment 'and the obligitory whitespace.' |
+    Write-RegEx -LiteralCharacter '<' |
+    Write-RegEx -Not -literalcharacter '>' -Repeat -Name Include |
+    Write-RegEx -Pattern '\>' |        
+    Set-Content -Path (Join-Path $myRoot $myName) -PassThru
