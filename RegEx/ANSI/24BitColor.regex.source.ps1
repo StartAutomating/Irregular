245bd6af5580d03ac308bdeeb3336ebd4650b350
James Brundage <@github.com>
2022-09-29 12:19:40 -0700

    ?<ANSI_8BitColor>/?<ANSI_24BitColor> : Support for Underline and BackgroundColor (Fixes #144 Fixes #146)

--- RegEx/ANSI/24BitColor.regex.source.ps1
+++ RegEx/ANSI/24BitColor.regex.source.ps1
@@ -4,7 +4,12 @@ $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 New-RegEx -Description "Matches an ANSI 24-bit color" -Modifier IgnoreCase -Not |
     New-RegEx -CharacterClass Escape -Comment 'An Escape' |
     New-RegEx -LiteralCharacter '[' -Comment 'Followed by a bracket' |
-    New-RegEx -Pattern '38;2;' |
+    New-RegEx -Atomic -Or @(
+        New-RegEx -Pattern '38' -Name IsForegroundColor
+        New-RegEx -Pattern '48' -Name IsBackgroundColor
+        New-RegEx -Pattern '58' -Name IsUnderlineColor
+    ) |
+    New-RegEx -Pattern ';2;' |
     New-RegEx -Name Color -Pattern (
         New-Regex -Name Red -Pattern '(?>[0-2][0-5][0-5]|[0-1]\d\d|\d{1,2})' -Comment 'Red is the first 0-255 value' |
         New-RegEx -Pattern ';' |
--- RegEx/ANSI/8BitColor.regex.source.ps1
+++ RegEx/ANSI/8BitColor.regex.source.ps1
@@ -4,7 +4,12 @@ $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 New-RegEx -Description "Matches an ANSI 8-bit color" -Modifier IgnoreCase -Not |
     New-RegEx -CharacterClass Escape -Comment 'An Escape' |
     New-RegEx -LiteralCharacter '[' -Comment 'Followed by a bracket' |
-    New-RegEx -Pattern '38;5;' |
+    New-RegEx -Atomic -Or @(
+        New-RegEx -Pattern '38' -Name IsForegroundColor
+        New-RegEx -Pattern '48' -Name IsBackgroundColor
+        New-RegEx -Pattern '58' -Name IsUnderlineColor
+    ) |
+    New-RegEx -Pattern ';5;' |
     New-RegEx -Name Color -Pattern (
         New-RegEx -Atomic -Or @(            
             New-Regex -Name StandardColor -Pattern '[0-7]' -Comment '0 -7 are standard colors' |
