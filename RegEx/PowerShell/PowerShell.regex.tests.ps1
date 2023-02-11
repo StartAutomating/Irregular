f29b932dcb797a99342b6894b9a2cac3cbd989bf
James Brundage <@github.com>
2023-02-10 17:25:43 -0800

    Adding PowerShell regex tests (re #182)

---
+++ RegEx/PowerShell/PowerShell.regex.tests.ps1
@@ -0,0 +1,15 @@
+describe 'PowerShell Regex' {
+    it 'Can match a #requires -Module' {
+        '#requires -module Irregular' |
+            ?<PowerShell_Requires> -Extract |
+            Select-Object -ExpandProperty Module |
+            Should -Be 'Irregular'
+    }
+
+    it 'Can match an open ended requirement' {
+        '#requires $psStyle' |        
+            ?<PowerShell_Requires> -Extract |
+            Select-Object -ExpandProperty Requirement |
+            Should -Be '$psStyle'
+    }
+}
