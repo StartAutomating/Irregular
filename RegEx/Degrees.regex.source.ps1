c8c0419d579302eb6677bcf2c65bfb58b47f900a
James Brundage <@github.com>
2023-04-12 19:36:37 -0700

    Adding ?<Degrees> (Fixes #185)

---
+++ RegEx/Degrees.regex.source.ps1
@@ -0,0 +1,20 @@
+﻿$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+
+New-RegEx -Description "Matching degrees" |
+    New-Regex -Pattern ?<Decimals> | 
+    New-RegEx -CharacterClass Whitespace -Min 0 -Comment "Optional Whitespace" |
+    New-RegEx -Atomic -Or @("Degrees","Degree", "°") -Comment "Degree(s) or the degree symbol" |
+    New-RegEx -CharacterClass Whitespace -Min 0 -Comment "Optional Whitespace" |
+    New-RegEx -Name UnitType -Optional -Pattern $(
+        New-RegEx -Atomic -Or @(
+            "Celsius"
+            "C"
+            "Fahrenheit"
+            "F"
+        )
+    ) -Comment "Optional unit" |
+    Set-Content -Path (Join-Path $myRoot $myName)
+
+
+
