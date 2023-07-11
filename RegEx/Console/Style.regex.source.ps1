ae547181be29aa09c38c5fc9909750c8ff54fa84
James Brundage <@github.com>
2023-07-10 22:57:46 -0700

    Renaming ANSI Regexes to Console (Fixes #188)
    Fixing nested references

--- RegEx/Console/Style.regex.source.ps1
+++ RegEx/Console/Style.regex.source.ps1
@@ -3,18 +3,18 @@ $myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
 
 New-RegEx -Description "Matches an ANSI style (color or text option)" |
     New-RegEx -Atomic -Or @(
-        New-RegEx -Pattern '?<ANSI_Reset>'
-        New-Regex -Pattern '?<ANSI_Bold>'
-        New-Regex -Pattern '?<ANSI_Blink>'
-        New-Regex -Pattern '?<ANSI_Faint>'
-        New-Regex -Pattern '?<ANSI_Italic>'
-        New-Regex -Pattern '?<ANSI_Invert>'
-        New-Regex -Pattern '?<ANSI_Hide>'
-        New-Regex -Pattern '?<ANSI_Strikethrough>'
-        New-Regex -Pattern '?<ANSI_Underline>'
-        New-RegEx -Pattern '?<ANSI_24BitColor>'
-        New-RegEx -Pattern '?<ANSI_8BitColor>'
-        New-RegEx -Pattern '?<ANSI_4BitColor>'        
-        New-RegEx -Pattern '?<ANSI_DefaultColor>'
+        New-RegEx -Pattern '?<Console_Reset>'
+        New-Regex -Pattern '?<Console_Bold>'
+        New-Regex -Pattern '?<Console_Blink>'
+        New-Regex -Pattern '?<Console_Faint>'
+        New-Regex -Pattern '?<Console_Italic>'
+        New-Regex -Pattern '?<Console_Invert>'
+        New-Regex -Pattern '?<Console_Hide>'
+        New-Regex -Pattern '?<Console_Strikethrough>'
+        New-Regex -Pattern '?<Console_Underline>'
+        New-RegEx -Pattern '?<Console_24BitColor>'
+        New-RegEx -Pattern '?<Console_8BitColor>'
+        New-RegEx -Pattern '?<Console_4BitColor>'        
+        New-RegEx -Pattern '?<Console_DefaultColor>'
     ) |    
     Set-Content -Path (Join-Path $myRoot $myName) -PassThru
