29fc36ebf0ee5aae51354630e9b0e7c30f9564c3
James Brundage <@github.com>
2023-07-10 22:34:55 -0700

    Adding ?<YAML_Value> (Fixes #192)
    Fixing child values

--- RegEx/YAML/Value.regex.ps1
+++ RegEx/YAML/Value.regex.ps1
@@ -36,7 +36,6 @@ New-RegEx -Description "Matches a YAML key" -Modifier Multiline -StartAnchor Lin
 (?m)^             # A key starts off a line
 (?<Indent>\s){0,} # Followed by whitespace
 (?<InList>\-\s)?  # Followed by an optional list start
-(?<Key>
 '@ +
     [Environment]::NewLine +
     $Key + 
@@ -45,16 +44,31 @@ New-RegEx -Description "Matches a YAML key" -Modifier Multiline -StartAnchor Lin
 New-RegEx -LiteralCharacter ':' -Comment 'Followed by colon'
 >#>
 @'
-)
 \: # Followed by colon
 )
 '@
 <#?<
 New-RegEx -Atomic -Or @(        
         New-RegEx -Atomic -Or @(
+        
+            New-RegEx -Name IsChild -Pattern (
+                New-RegEx -Before '[\r\n]{1,2}\k<Indent>\s{2}'
+            ) -Optional |
+                New-RegEx -Name Value (
+                    New-RegEx -NoCapture -Min 1 (
+                        New-RegEx -Pattern '[\r\n]{1,2}' |
+                        New-RegEx -Backreference Indent |
+                        New-RegEx -Atomic -Or @(
+                            New-RegEx -Pattern '\-\s'
+                            New-RegEx -Pattern '\s{2}'
+                        ) |
+                        New-RegEx -Pattern '[^\r\n]{1,}'
+                    )
+                )
+
             New-RegEx -Pattern "[\s-[\r\n]]" -Comment "Inline values start with a single space" |
                 New-Regex -Atomic -Or @(
-                    New-RegEx -Name IsBlock -Pattern '\|(?>\+|\-)?(?=[\r\n]{1,2})' |
+                    New-RegEx -Name IsBlock -Pattern '\|(?>\+|\-)?(?=[\r\n]{1,2})' -Optional |
                     New-RegEx -Name Value (
                         New-RegEx -NoCapture -Min 1 (
                             New-RegEx -Pattern '[\r\n]{1,2}' |
@@ -66,6 +80,8 @@ New-RegEx -Atomic -Or @(
                             New-RegEx -Pattern '[^\r\n]{1,}'
                         )
                     )
+
+                    
 
                     New-RegEx -Name ValueInline (
                         New-RegEx -Not -LiteralCharacter "'`"" -CharacterClass NewLine, CarriageReturn -Repeat -Name Value -Comment "An unquoted value"
@@ -91,9 +107,12 @@ New-RegEx -Atomic -Or @(
 @'
 (?>
   (?>
+  (?<IsChild>(?=[\r\n]{1,2}\k<Indent>\s{2}))?(?<Value>(?:[\r\n]{1,2}\k<Indent>(?>
+  \-\s  |
+  \s{2})[^\r\n]{1,}){1,})  |
   [\s-[\r\n]]                                   # Inline values start with a single space
 (?>
-  (?<IsBlock>\|(?>\+|\-)?(?=[\r\n]{1,2}))(?<Value>(?:[\r\n]{1,2}\k<Indent>(?>
+  (?<IsBlock>\|(?>\+|\-)?(?=[\r\n]{1,2}))?(?<Value>(?:[\r\n]{1,2}\k<Indent>(?>
   \-\s  |
   \s{2})[^\r\n]{1,}){1,})  |
   (?<ValueInline>(?<Value>[^\n\r\'\"]+)         # An unquoted value
