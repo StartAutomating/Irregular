e3fdfbb03e1880c6b38d4507bc028a2eb3cf0442
James Brundage <@github.com>
2024-04-15 19:21:32 -0700

    feat: ?<Symbol_Currency> ( Fixes #214 )

---
+++ RegEx/Symbol/Currency.regex.source.ps1
@@ -0,0 +1,9 @@
+$myName = ($MyInvocation.MyCommand.ScriptBlock.File | Split-Path -Leaf) -replace '\.source', '' -replace '\.ps1', '.txt'
+$myRoot = $MyInvocation.MyCommand.ScriptBlock.File | Split-Path
+
+New-RegEx -Description "Matches a Math Symbol" |
+    New-RegEx -CharacterClass SymbolMath |
+    Set-Content -Path (Join-Path $myRoot $myName)
+
+
+
