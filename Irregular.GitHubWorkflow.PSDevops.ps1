de1041ea1f075bfe26fae4ebcedf61f7804cd043
James Brundage <@github.com>
2024-04-15 17:45:32 -0700

    feat: https://ghcr.io/startautomating/irregular ( Fixes #207 )

--- Irregular.GitHubWorkflow.PSDevops.ps1
+++ Irregular.GitHubWorkflow.PSDevops.ps1
@@ -2,14 +2,18 @@
 Push-Location $PSScriptRoot
 
 Import-BuildStep -ModuleName Irregular
-New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -Job PowerShellStaticAnalysis, TestPowerShellOnLinux, TagReleaseAndPublish, BuildIrregular -On Push, Demand |
-    Set-Content .\.github\workflows\IrregularTests.yml -Encoding UTF8 -PassThru
+New-GitHubWorkflow -Name "Test, Tag, Release, and Publish" -Job PowerShellStaticAnalysis,
+    TestPowerShellOnLinux,
+    TagReleaseAndPublish,
+    BuildIrregular -On Push, Demand -Environment ([Ordered]@{
+        REGISTRY = 'ghcr.io'
+        IMAGE_NAME = '${{ github.repository }}'
+    }) -OutputPath .\.github\workflows\IrregularTests.yml
 
-New-GitHubWorkflow -Name "Run GitHub Action" -On Push, Demand -Job UseIrregularAction |
-    Set-Content .\.github\workflows\RunIrregularAction.yml -Encoding UTF8 -PassThru
+New-GitHubWorkflow -Name "Run GitHub Action" -On Push,
+    Demand -Job UseIrregularAction -OutputPath .\.github\workflows\RunIrregularAction.yml
 
-New-GitHubWorkflow -On Issue, Demand -Job RunGitPub -Name OnIssueChanged |
-    Set-Content (Join-Path $PSScriptRoot .github\workflows\OnIssue.yml) -Encoding UTF8 -PassThru
+New-GitHubWorkflow -On Demand -Job RunGitPub -Name "GitPub" -OutputPath .\.github\workflows\RunGitPub.yml
     
 
 Pop-Location
