279a9d94315e73a066e82da3fb5118e30d33391d
James Brundage <@github.com>
2024-04-15 18:06:32 -0700

    chore: Moving Irregular.AdoPipeline into /Build ( Fixes #209 )

---
+++ Build/Irregular.ADOPipeline.PSDevOps.ps1
@@ -0,0 +1,8 @@
+﻿#requires -Modules PSDevOps
+
+Push-Location ($PSScriptRoot | Split-Path)
+
+New-ADOPipeline -Stage PowerShellStaticAnalysis, TestPowerShellCrossPlatform, UpdatePowerShellGallery |
+    Set-Content .\azure-pipelines.yml -Encoding UTF8
+
+Pop-Location
--- Irregular.ADOPipeline.PSDevOps.ps1
+++
@@ -1,9 +0,0 @@
-﻿<#
-.Synopsis
-    
-.Description
-    
-#>
-#requires -Modules PSDevOps
-New-ADOPipeline -Stage PowerShellStaticAnalysis, TestPowerShellCrossPlatform, UpdatePowerShellGallery |
-    Set-Content .\azure-pipelines.yml -Encoding UTF8
